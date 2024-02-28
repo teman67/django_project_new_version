20d590e0f32a2514eefe6a5deea37ef48fbbb464
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-02-28 22:20:16 +0000

    add urls to app folder

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,8 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    # queryset = Post.objects.filter(author=2)
+    # queryset = Post.objects.all().order_by("-create_on")
+    # queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    template = "post_list.html"
