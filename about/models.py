a73bce034c501d0a139fa410d3eec60bbff3c2d3
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-03-04 11:59:54 +0000

    fix the about template dir

--- about/models.py
+++ about/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
+
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
