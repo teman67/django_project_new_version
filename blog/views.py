3139d80e5d42eb19149afbb672cd39d928719229
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-03-05 12:23:57 +0000

    add print steatment in views.py

--- blog/views.py
+++ blog/views.py
@@ -34,6 +34,7 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-create_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -45,6 +46,8 @@ def post_detail(request, slug):
             'Comment submitted and awaiting approval'
         )
     comment_form = CommentForm()
+
+    print("About to render template")
 
     return render(
         request,
